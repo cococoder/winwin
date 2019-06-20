32a86363ec56a3729abb87136b991fe140aa645f
Coco Coder <shout@cococoder.com>
2019-06-20 18:18:48 +0300

    Add the deal_ price

--- lib/winwin.rb
+++ lib/winwin.rb
@@ -2,5 +2,23 @@ require "winwin/version"
 
 module Winwin
   class Error < StandardError; end
-  # Your code goes here...
+  
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :api_url, :token,:tag
+
+    def initialize
+      @api_url = 'http://raiiar.com/api'
+      @token = "r4114r"
+      @tag = "raiiar"
+    end
+  end# Your code goes here...
 end
--- spec/winwin_spec.rb
+++ spec/winwin_spec.rb
@@ -1,5 +1,69 @@
 RSpec.describe Winwin do
-  it 'should all be ok' do
-    expect(true).to eq(true)
+ describe "Configuration" do
+    it 'should know the apir url' do
+
+      api_url = "http://raiiar.com/api"
+
+      Winwin.configure do |config|
+        expect(api_url).to eq(config.api_url)
+      end
+
+    end
+    it 'should know token' do
+
+      token = "r4114r"
+
+      Winwin.configure do |config|
+        expect(token).to eq(config.token)
+      end
+
+    end
+    it 'should know the tag' do
+
+      tag = "raiiar"
+
+      Winwin.configure do |config|
+        expect(tag).to eq(config.tag)
+      end
+
+    end
+
+  end
+
+ class Negotiation
+   attr_accessor :deal_price 
+   def initialize
+     ok!
+   end
+
+   def ok?
+     @ok
+   end
+
+   def ok!
+     @ok = true
+   end
+
+   def ko!
+     @ok = false
+   end
+ end
+  describe "Negotiation" do
+    it "should know when ok" do
+      negotiation = Negotiation.new 
+      expect(true).to eq negotiation.ok?
+    end
+
+    it "should know when ko" do
+      negotiation = Negotiation.new 
+      negotiation.ko!
+      expect(false).to eq negotiation.ok?
+    end
+
+    it "should know the deal price" do
+      negotiation = Negotiation.new
+      negotiation.deal_price = 10.00
+      expect(10.00).to eq negotiation.deal_price
+    end
   end
 end
